c69fd07d4bcaf71ed225331abf8f850bd64e0223
Carlos <carmargut@gmail.com>
2017-03-18 15:50:11 +0100

    https test

--- app.js
+++ app.js
@@ -2,20 +2,22 @@
 
 var path = require("path");
 var express = require("express");
-
 var config = require('./config')
 var fs = require("fs");
 var https = require("https");
 var app = express();
+
+var clavePrivada = fs.readFileSync(config.private_key).toString();
+var certificado = fs.readFileSync(config.certificate).toString();
+var ca_bundle = fs.readFileSync(config.ca_bundle).toString();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 
-var clavePrivada = fs.readFileSync(config.private_key);
-var certificado = fs.readFileSync(config.certificate);
-var servidor = https.createServer({ key: clavePrivada, cert: certificado }, app);
+
+var servidor = https.createServer({ key: clavePrivada, ca: ca_bundle, cert: certificado }, app);
 
 app.get("/", function(request, response) {
     response.render("index",{});
